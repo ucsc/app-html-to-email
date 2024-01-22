830317bf85c3e9b7fcc49fff94ef923fe23b8e7f
Rob Knight <raknight@ucsc.edu>
2024-01-22 12:10:20 -0800

    feat: ⚡️ Add a proper front page, error messages and a domain check

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const compression = require("compression");
 const express = require("express");
+const flash = require('express-flash');
+const session = require('express-session');
 const app = express();
 const port = process.env.PORT || 5500;
 
+// Session setup
+app.use(session({
+    secret: process.env.SESSION_SECRET, // Replace with a real secret in production
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: process.env.SECURE_COOKIE || true } // Set to true if using https
+}));
+
 app.use(compression());
+app.use(flash());
 app.use(express.json({ extended: false }));
 app.use("/", require("./routes/index"));
+
+app.set('view engine', 'ejs');
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
--- routes/index.js
+++ routes/index.js
@@ -15,23 +15,29 @@ router.get("/*", async (req, res) => {
     const { url } = req.query;
     const site = new URL(url);
 
-    try {
-      const response = await fetch(url);
-      const data = await response.text();
-      const html = fixUrls(data, site.origin);
+    if (site.hostname.endsWith('ucsc.edu')) {
+      
+      try {
+        const response = await fetch(url);
+        const data = await response.text();
+        const html = fixUrls(data, site.origin);
 
-      inlineCss(html, { url: site.origin, removeLinkTags: false }).then(function(html) {
-        res.send(`<h2 style="text-align:center;margin:2rem auto;">Code</h2><div style="margin:1rem auto; width:50%;"><textarea rows="10" style="width:100%;">${html}</textarea></div><h2 style="text-align:center;margin:2rem auto;">Preview</h2>${html}`);  
-      }).catch(function (err) {
-        console.log("Error: " + err);
-      });        
-    } catch (error) {
-        res.status(500).send(`Error fetching URL: ${error.message}`);
-    }
+        inlineCss(html, { url: site.origin, removeLinkTags: false }).then(function(html) {
+          res.send(`<h2 style="text-align:center;margin:2rem auto;">Code</h2><div style="margin:1rem auto; width:50%;"><textarea rows="10" style="width:100%;">${html}</textarea></div><h2 style="text-align:center;margin:2rem auto;">Preview</h2>${html}`);  
+        }).catch(function (err) {
+          console.log("Error: " + err);
+        });        
+      } catch (error) {
+          res.status(404).send(`Unable to fetch URL or prepare the code: ${error.message}`);
+      }
     
+    } else {
+        req.flash('info', `Your URL must be a ucsc.edu address`);
+        res.render('index', { title: 'Email Preparer' });
+    }  
+
   } else {
-    
-    return res.status(200).send('A URL is required as a query parameter. In your browser\'s URL bar, add <code style="padding:.5ch; background-color:yellow;">?url=</code> followed by a Tuesday Newsday URL to get the prepared code.');
+    res.render('index', { title: 'Email Preparer' });
   
   }
 });
